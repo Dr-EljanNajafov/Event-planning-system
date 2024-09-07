c3bdda60fd3be213f43e009554a8e5afb346262b
Eljan Najafov <eljan.najafov02@mail.ru>
2024-09-07 14:22:05 +0400

    init

---
+++ src/main/java/com/github/eventplanningsystem/EventPlanningSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.eventplanningsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPlanningSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventPlanningSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/eventplanningsystem/EventPlanningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eventplanningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPlanningSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
