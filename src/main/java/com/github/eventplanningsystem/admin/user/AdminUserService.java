5f512b3f0d4196219051e3bd38408dcbda2409e2
Eljan Najafov <eljan.najafov02@mail.ru>
2024-09-10 08:34:42 +0400

    working with user ADMIN

--- src/main/java/com/github/eventplanningsystem/admin/user/AdminUserService.java
+++ src/main/java/com/github/eventplanningsystem/admin/user/AdminUserService.java
@@ -1,106 +1,71 @@
-//package com.github.eventplanningsystem.admin.user;
-//
-//import com.github.eventplanningsystem.admin.event.AdminEventService;
-//import com.github.eventplanningsystem.admin.report.AdminReportService;
-//import com.github.eventplanningsystem.event.Event;
-//import com.github.eventplanningsystem.user.*;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.server.ResponseStatusException;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class AdminUserService {
-//    private final UserRepository userRepository;
-//    private final UserService userService;
-//    private final AdminEventService eventService;
-//    private final PasswordEncoder passwordEncoder;
-//    private final AdminReportService reportService;
-//
-//    public List<Long> users(GetUserRequest request) {
-//        if (request.getStart() < 0 || request.getCount() < 0)
-//            throw new ResponseStatusException(
-//                    HttpStatus.BAD_REQUEST,
-//                    "'start' and 'count' must be > 0"
-//            );
-//
-//        List<UserE> users = userRepository.findAll();
-//        return users
-//                .subList(Math.min(request.getStart(), users.size()), Math.min(request.getStart() + request.getCount(), users.size()))
-//                .stream()
-//                .map(UserE::getId)
-//                .toList();
-//    }
-//
-//    public UserDto userInfo(long id) {
-//        UserE user = user(id);
-//        return new UserDto(
-//                user.getId(),
-//                user.getUsername(),
-//                user.getRole()
-//        );
-//    }
-//
-//    public UserE user(long id) {
-//        return userRepository.findById(id)
-//                .orElseThrow(() -> new ResponseStatusException(
-//                        HttpStatus.NOT_FOUND,
-//                        "User with id %d doesn't exist".formatted(id)
-//                ));
-//    }
-//    public UserDto registerUser(RegisterByAdminRequest request) {
-//        userService.checkUsername(request.getUsername());
-//
-//        UserE user = UserE
-//                .builder()
-//                .role(request.isAdmin() ? Role.ADMIN : Role.USER)
-//                .username(request.getUsername())
-//                .password(request.getPassword())
-//                .build();
-//        userRepository.save(user);
-//        return userInfo(user.getId());
-//    }
-//
-//    public UserDto updateUser(long id, UpdateByAdminRequest request) {
-//        checkUsername(request.getUsername(), id);
-//        UserE user = UserE
-//                .builder()
-//                .role(request.isAdmin() ? Role.ADMIN : Role.USER)
-//                .username(request.getUsername())
-//                .password(request.getPassword())
-//                .build();
-//        userRepository.save(user);
-//        return userInfo(user.getId());
-//    }
-//
-//    public void deleteUser(long id) {
-//        UserE user = user(id);
-//
-//        userRepository.delete(user);
-//    }
-//
-//    // Check if username is already in use
-//    public void checkUsername(String username, long id) {
-//        Optional<UserE> userOptional = userRepository.findByUsername(username);
-//        if (userOptional.isPresent() && !userOptional.get().getId().equals(id)) {
-//            throw new ResponseStatusException(
-//                    HttpStatus.BAD_REQUEST,
-//                    "Username '%s' is already in use".formatted(username)
-//            );
-//        }
-//    }
-//
-//    public void checkUsername(String username) {
-//        if (userRepository.findByUsername(username).isPresent()) {
-//            throw new ResponseStatusException(
-//                    HttpStatus.BAD_REQUEST,
-//                    "Username '%s' is already in use".formatted(username)
-//            );
-//        }
-//    }
-//}
+package com.github.eventplanningsystem.admin.user;
+
+import com.github.eventplanningsystem.admin.event.AdminEventService;
+import com.github.eventplanningsystem.admin.report.AdminReportService;
+import com.github.eventplanningsystem.event.Event;
+import com.github.eventplanningsystem.user.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AdminUserService {
+    private final UserRepository userRepository;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    public List<Long> users(GetUserRequest request) {
+        if (request.getStart() < 0 || request.getCount() < 0)
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "'start' and 'count' must be > 0"
+            );
+
+        List<UserE> users = userRepository.findAll();
+        return users
+                .subList(Math.min(request.getStart(), users.size()), Math.min(request.getStart() + request.getCount(), users.size()))
+                .stream()
+                .map(UserE::getId)
+                .toList();
+    }
+
+    public UserDto userInfo(String username) {
+        return userService.userInfo(username);
+    }
+
+    public UserDto registerUser(RegisterByAdminRequest request) {
+        userService.checkUsername(request.getUsername());
+
+        UserE user = UserE
+                .builder()
+                .role(request.isAdmin() ? Role.ADMIN : Role.USER)
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        userRepository.save(user);
+        return userInfo(user.getUsername());
+    }
+
+    public UserDto updateUser(String username, UpdateByAdminRequest request) {
+        userService.checkUsername(request.getUsername(), username);
+
+        UserE user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        // Обновляем поля пользователя
+        user.setRole(request.isAdmin() ? Role.ADMIN : Role.USER);
+        user.setUsername(request.getUsername());
+        if (request.getPassword() != null && !request.getPassword().isEmpty()) {
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+
+        userRepository.save(user);
+        return userInfo(user.getUsername());
+    }
+}
--- src/main/java/com/github/eventplanningsystem/admin/user/DefaultAdminLoader.java
+++ src/main/java/com/github/eventplanningsystem/admin/user/DefaultAdminLoader.java
@@ -1,14 +1,14 @@
-//package com.github.eventplanningsystem.admin.user;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-//
-////Need only for test
-//@Component
-//public class DefaultAdminLoader {
-//    @Autowired
-//    public DefaultAdminLoader(AdminUserService service) {
-//        service.registerUser(new RegisterByAdminRequest("admin", "admin", true));
-//    }
-//}
+package com.github.eventplanningsystem.admin.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+//Need only for test
+@Component
+public class DefaultAdminLoader {
+    @Autowired
+    public DefaultAdminLoader(AdminUserService service) {
+        service.registerUser(new RegisterByAdminRequest("admin", "admin", true));
+    }
+}
